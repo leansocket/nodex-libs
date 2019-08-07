e8dda6e4522e4401a8974df49215beb0d558b878
chengqilin <qilin.cheng@foxmail.com>
2019-08-07 21:23:51 +0800

    new module ‘dt’ for DateTime

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let load = function(name){
 };
 
 let modules = [
-    'log', 'fmt', 'iters', 'schedule', 'flakes',
+    'log', 'fmt', 'iters', 'dt', 'schedule', 'flakes',
     'http', 'smtp', 'db',
     'vfs', 'crypto', 'authes', 'html',
     'util',
