a93e56a8d38a262c08855cfd5b4f0500ff89e703
chengqilin <qilin.cheng@foxmail.com>
2019-08-24 12:24:03 +0800

    1.3.3：https & http2

--- libs/http.js
+++ libs/http.js
@@ -1,5 +1,6 @@
 
 let http = require('http');
+let https = require('https');
 let http2 = require('http2');
 let liburl = require('url');
 let query = require('querystring');
@@ -116,7 +117,7 @@ let doGet = function(args, data, callback) {
         callback = data;
     }
 
-    let proto = safe ? http2 : http;
+    let proto = safe ? https : http;
 
     let req = proto.request(options, (res)=>{
         return onRes(res, callback);
@@ -187,7 +188,7 @@ let doPost = function(args, data, callback) {
         callback = data;
     }
 
-    let proto = safe ? http2 : http;
+    let proto = safe ? https : http;
 
     let req = proto.request(options, (res)=>{
         onRes(res, callback);
@@ -327,8 +328,14 @@ exports.webapp = function(args) {
             let key = fs.readFileSync(vfs.absolutePath(args.https_key));
             let options = {cert: cert, key: key};
 
-            let server = http2.createServer(options, app.callback());
-            server.listen.apply(server, arguments);
+            if(args.http2 === true) {
+                let server = http2.createSecureServer(options, app.callback());
+                server.listen.apply(server, arguments);
+            }
+            else {
+                let server = https.createServer(options, app.callback());
+                server.listen.apply(server, arguments);
+            }
         };
     }
 
