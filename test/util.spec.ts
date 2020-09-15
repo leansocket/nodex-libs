42435975976ec9168639d5f4f31fcfe1c8f668e0
pstXZ <pstzpw@163.com>
2020-09-15 17:21:36 +0800

    test：添加util.ts的单元测试

---
+++ test/util.spec.ts
@@ -0,0 +1,29 @@
+import { camelCase, camelCaseKeys, computeGeoDistance } from '../src/util';
+
+test('test camelCase function', () => {
+  let val = camelCase('get_user_info');
+  let val2 = camelCase('_get_user_info');
+  expect(val).toBe('getUserInfo');
+  expect(val2).toBe('GetUserInfo');
+})
+
+test('test camelCaseKeys function', () => {
+  let obj = {   
+    get_user_info: 'zpw',
+    get_list: "zpw"
+  };
+  let retVal = camelCaseKeys(obj);
+  let arr = [];
+
+  for (const key in retVal) {
+    arr.push(key);
+  }
+
+  expect(arr[0]).toBe('getUserInfo');
+  expect(arr[1]).toBe('getList');
+})
+
+test('test computeGeoDistance function', () => {
+  let distance = computeGeoDistance(10, 10, 20, 20);
+  expect(distance).toBe(1546.4880483491936);
+})
