6bd75fe0f83ba183dc36beb591346ed0c83144a9
pstXZ <pstzpw@163.com>
2020-09-15 10:33:09 +0800

    fix: 修复spawn.spec.ts  \r\n 兼容问题

--- test/spawn.spec.ts
+++ test/spawn.spec.ts
@@ -1,10 +1,11 @@
 import { SpawnedProcess } from '../src/spawn';
+import os from 'os';
 
 test('spawn a child process and exec commands on it.', () => {
     const spawn = new SpawnedProcess({cwd: process.cwd(), env: process.env})
     spawn.post('echo',['hello word'], function(code, out, err) {
         expect(code).toBe(0)
-        expect(out).toBe("hello word\n")
+        expect(out).toBe("hello word"+os.EOL)
         expect(err).toBe('')
     });
 });
