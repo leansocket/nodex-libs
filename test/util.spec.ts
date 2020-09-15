e9a7c2e85e5355a364c0b8205b9aae7f9e03252b
ruby.luo <ruby.luo@xgimi.com>
2020-09-15 22:29:32 +0800

    test: 增加absolutePath测试

--- test/util.spec.ts
+++ test/util.spec.ts
@@ -1,16 +1,22 @@
-import { camelCase, camelCaseKeys, computeGeoDistance } from '../src/util';
+import path from "path";
+import {
+  camelCase,
+  camelCaseKeys,
+  computeGeoDistance,
+  absolutePath,
+} from "../src/util";
 
-test('test camelCase function', () => {
-  let val = camelCase('get_user_info');
-  let val2 = camelCase('_get_user_info');
-  expect(val).toBe('getUserInfo');
-  expect(val2).toBe('GetUserInfo');
-})
+test("test camelCase function", () => {
+  let val = camelCase("get_user_info");
+  let val2 = camelCase("_get_user_info");
+  expect(val).toBe("getUserInfo");
+  expect(val2).toBe("GetUserInfo");
+});
 
-test('test camelCaseKeys function', () => {
-  let obj = {   
-    get_user_info: 'zpw',
-    get_list: "zpw"
+test("test camelCaseKeys function", () => {
+  let obj = {
+    get_user_info: "zpw",
+    get_list: "zpw",
   };
   let retVal = camelCaseKeys(obj);
   let arr = [];
@@ -19,11 +25,16 @@ test('test camelCaseKeys function', () => {
     arr.push(key);
   }
 
-  expect(arr[0]).toBe('getUserInfo');
-  expect(arr[1]).toBe('getList');
-})
+  expect(arr[0]).toBe("getUserInfo");
+  expect(arr[1]).toBe("getList");
+});
 
-test('test computeGeoDistance function', () => {
+test("test computeGeoDistance function", () => {
   let distance = computeGeoDistance(10, 10, 20, 20);
   expect(distance).toBe(1546.4880483491936);
-})
+});
+
+test("test absolutePath function", () => {
+  const p = absolutePath("/test/util.spec.ts");
+  expect(p).toBe(path.resolve(process.cwd(), "/test/util.spec.ts"));
+});
