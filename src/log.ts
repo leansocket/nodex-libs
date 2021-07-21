1b7e575616907ae4d42703aaab58186c4e62acd1
RubyRose <840867182@qq.com>
2021-07-21 22:30:41 +0800

    feat: 支持远程上报日志

--- src/log.ts
+++ src/log.ts
@@ -1,17 +1,85 @@
-
+import util from 'util';
 import chalk from 'chalk';
+import { post } from './http/client';
+import { sign } from './http/basic';
+
+/**
+ * 日志模块回调函数
+ * @param {string} scope 日志的前缀标签
+ * @param {string} message 日志内容
+ */
+export type LogHandlerInfo = (scope: string, message: string) => void;
+
+/**
+ * 日志服务器配置参数
+ */
+export interface LogServerInfo {
+	/**
+	 * 日志服务器链接
+	 */
+	url: string,
+
+	/**
+	 * Appid
+	 */
+	appid: string,
+
+	/**
+	 * 密钥
+	 */
+	secret: string
+}
+
+/**
+ * 日志配置
+ */
+export interface LogOptions {
+	/**
+	 * 日志的前缀标签
+	 */
+	scope: string,
+
+	/**
+	 * 服务器配置
+	 */
+	server?: LogServerInfo,
+
+	/**
+	 * 日志回调
+	 */
+	handler?: LogHandlerInfo
+}
+
+/**
+ * 日志内容参数
+ */
+interface LogInfo {
+	time: string,
+	level: string,
+	content: string
+}
 
 /**
  * 初始化日志模块
  * @param {string} scope 日志的前缀标签
 */
-export const init = function (scope: string): void {
+export const init = function (options: string | LogOptions): void {
 
-	let pad2 = function (val) {
+	let scope, server, handler;
+
+	if (typeof options === 'string') {
+		scope = options;
+	} else {
+		scope = options.scope;
+		server = options.server;
+		handler = options.handler;
+	}
+
+	const pad2 = function (val) {
 		return val < 10 ? `0${val}` : val;
 	};
 
-	let time = function () {
+	const getTime = function () {
 		let t = new Date();
 		let Y = t.getFullYear();
 		let M = pad2(t.getMonth() + 1);
@@ -19,61 +87,65 @@ export const init = function (scope: string): void {
 		let h = pad2(t.getHours());
 		let m = pad2(t.getMinutes());
 		let s = pad2(t.getSeconds());
-		return `${Y}/${M}/${D} ${h}:${m}:${s}`;
+		return `${Y}-${M}-${D} ${h}:${m}:${s}`;
 	}
 
-	let tostr = function (message) {
-		if (message === undefined) {
-			return 'undefined';
-		}
-		if (message === null) {
-			return 'null';
-		}
-		let type = typeof (message);
-		if (type === 'object') {
-			return JSON.stringify(message);
-		}
-		return message.toString();
-	};
+	const sendLog = async function (logInfo: LogInfo) {
 
-	let tolog = function (level, ...message) {
-		let logstr = `${level}> ${time()} [${scope}]`;
-		message.forEach(msg => {
-			logstr += ` ${tostr(msg)}`;
-		});
-		return logstr;
-	};
+		const { appid, secret, url } = server;
 
-	let configs = {
+		const data = {
+			...logInfo,
+			scope,
+			appid,
+		}
+		data['$_appid'] = appid;
+		data['$_sign'] = sign(data, secret);
+		return post(url, data);
+	}
+
+	const configs = {
 		log: {
-			level: 'L',
+			level: 'LOG',
 			style: chalk.white
 		},
 		info: {
-			level: 'I',
+			level: 'INFO',
 			style: chalk.green
 		},
 		notice: {
-			level: 'N',
+			level: 'NOTICE',
 			style: chalk.blue
 		},
 		warn: {
-			level: 'W',
+			level: 'WARN',
 			style: chalk.yellow.bold
 		},
 		error: {
-			level: 'E',
+			level: 'ERROR',
 			style: chalk.red.bold
 		}
 	};
 
-	let console_log = console.log;
+	let log = console.log;
 
 	for (let x in configs) {
 		let cfg = configs[x];
+		const time = getTime();
+		const level = cfg.level
 		console[x] = function () {
-			let str = tolog(cfg.level, ...arguments);
-			console_log(cfg.style(str));
-		};
+			let content = `[${scope}] ${time}|${level}| ${util.format.apply(this, arguments as any)}`
+			log(cfg.style(content));
+			if (handler) {
+				handler(scope, content);
+			}
+			if (server) {
+				sendLog({
+					time,
+					level,
+					content: util.format.apply(this, arguments as any)
+				})
+			}
+		}
 	}
 };
